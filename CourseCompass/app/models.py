5f4c657fd5f2e5aefea54900412e2018b0bf0704
Michael McGrath <mmcgra13@students.kennesaw.edu>
2025-04-20 18:50:51 -0400

    Update models.py
    wrote code for database model

--- CourseCompass/app/models.py
+++ CourseCompass/app/models.py
@@ -1,1 +1,14 @@
+from app import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Grade(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.String(100), nullable=False)
+    grade = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
