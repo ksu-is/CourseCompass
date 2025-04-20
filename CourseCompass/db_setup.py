0d0fc085bbfdce6c4ab3d687bf941f6f129c70fd
Michael McGrath <mmcgra13@students.kennesaw.edu>
2025-04-20 19:03:15 -0400

    Wrote Database script

--- CourseCompass/db_setup.py
+++ CourseCompass/db_setup.py
@@ -1,1 +1,5 @@
-
+from app import db
+db.create_all()
+print("Database created!")
+# This code creates the database and tables defined in the models.py file.
+# It uses the SQLAlchemy ORM to create the database schema based on the models defined in the app.
