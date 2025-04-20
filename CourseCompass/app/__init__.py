8e5f93d9f0fbe1d880fa809e73379f54f3bc464a
Michael McGrath <mmcgra13@students.kennesaw.edu>
2025-04-20 18:50:03 -0400

    Update __init__.py
    Wrote code for initialization of the app.

--- CourseCompass/app/__init__.py
+++ CourseCompass/app/__init__.py
@@ -1,1 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grades.db'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+from app import routes
 
