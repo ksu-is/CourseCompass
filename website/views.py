2bef76495ae22558b0db9df4b9650067b12737d0
Michael McGrath <mmcgra13@students.kennesaw.edu>
2025-04-20 18:05:59 -0400

    Delete website directory
    Restarting folder hierarchy as I could not update it.

--- website/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-views = Blueprint('views', __name__)
-
-@views.route('/', methods=['GET'])
-def home():
-    return "<h1 style='color: green;'>Hello from localhost!</h1>"
