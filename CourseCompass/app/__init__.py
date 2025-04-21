c67a9a789d3f9bb667b6d9e22264f2eed48ced78
Michael McGrath <mmcgra13@students.kennesaw.edu>
2025-04-21 19:02:37 -0400

    Configured Flask-Mail and updated app credentials

--- CourseCompass/app/__init__.py
+++ CourseCompass/app/__init__.py
@@ -19,7 +19,6 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'michael.mcgrath.2024@gmail.com'  # your actual Gmail address
 app.config['MAIL_PASSWORD'] = 'fito inrw nvpr tadj'  # <- Paste the 16-char app password here
 
-
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
