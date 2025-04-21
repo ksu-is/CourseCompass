a227788a6d5fbafa22f363ac58fee91839e51736
Michael McGrath <mmcgra13@students.kennesaw.edu>
2025-04-21 17:55:18 -0400

    Adding email functionality

--- CourseCompass/app/__init__.py
+++ CourseCompass/app/__init__.py
@@ -12,3 +12,24 @@ login_manager.login_view = 'login'
 
 from app import routes
 
+from flask_mail import Mail
+
+mail = Mail()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/grades.db'
+
+    # Email config
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'your-email@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'your-app-password'
+
+    db.init_app(app)
+    mail.init_app(app)
+
+    from app import routes
+    return app
