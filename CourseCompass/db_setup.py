3415789b7b9af10704361a1fc5717854e7fd1915
Michael McGrath <mmcgra13@students.kennesaw.edu>
2025-04-21 19:36:34 -0400

    Database Update

--- CourseCompass/db_setup.py
+++ CourseCompass/db_setup.py
@@ -1,8 +1,7 @@
-from app import db, app
+from app import db, create_app
+
+app = create_app()
 
 with app.app_context():
     db.create_all()
     print("Database created!")
-
-# This code creates the database and tables defined in the models.py file.
-# It uses the SQLAlchemy ORM to create the database schema based on the models defined in the app.
